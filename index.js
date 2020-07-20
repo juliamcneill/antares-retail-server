85f54319f4712aa5d9f1357124ab4ed7bbed9ec6
Julia McNeill <juliammcneill@gmail.com>
2020-07-19 23:12:42 -0400

    Import reviews data and connect routes to database

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose
-  .connect("mongodb://mongo:27017/node", { useNewUrlParser: true })
-  .then(() => {
+  .connect("mongodb://mongo:27017/reviews-api", { useNewUrlParser: true })
+  .then((error, database) => {
     console.log("Connection to database was successful!");
   })
   .catch((error) => {
     console.log(error);
   });
 
-const Reviews = require("./db/schema.js").Reviews;
+var Schema = require("./db/schema.js").ReviewsSchema;
+var Reviews = mongoose.model("Reviews", Schema, "reviews");
 
 app.get("/", (req, res) => {
-  Reviews.find()
-    .then(() => {
-      res.status(200).send("Success");
+  Reviews.find({ id: 1 })
+    .then((records) => {
+      res.status(200).send(records);
     })
     .catch((error) => {
       res.status(404).send(error);
